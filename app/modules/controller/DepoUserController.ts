72b35c39578c7e9ddfd700ad0b88f8443bdfe4eb
bitmaster001 <superora0002@gmail.com>
2021-09-02 15:17:39 -0300

    feat: add api keys to the user object

--- app/modules/controller/DepoUserController.ts
+++ app/modules/controller/DepoUserController.ts
@@ -4,6 +4,9 @@ import { IUser } from "../interfaces/IUser";
 import { respond } from "../util/respond";
 import { IResponse } from "../interfaces/IResponse";
 import { AbstractEntity } from "../abstract/AbstractEntity";
+import { IAPIKey } from "../interfaces/IAPIKey";
+import { CryptoJsHandler } from "../util/crypto-js";
+import { IWallet } from "../interfaces/IWallet";
 
 /**
  * This is the model controller class.
@@ -80,7 +83,10 @@ export class DepoUserController extends AbstractEntity {
   }
 
   /**
-   * Updates a user
+   * Updates a user.
+   * 
+   * _Note that this will only add objects to the array, not delete. To delete, use `removeWallet` or `removeExchange` methods._
+   * 
    * @param walletId main user's wallet id
    */
   async update(walletId: string): Promise<void | IResponse> {
@@ -89,12 +95,22 @@ export class DepoUserController extends AbstractEntity {
       if (dbm) {
         const collection = dbm.collection(this.table);
         const hasUser = await this.findUser(walletId);
+        // Verify if has any error while finding user
         if (!hasUser.code) {
+          // if not mount the query to update an user
           const filter = this.findUserQuery(walletId);
+          // mount the $set query
           const updateDoc = this.moundUpdateUserDocument(hasUser);
-          await collection.updateOne(filter, updateDoc);
+          await collection.updateOne(filter, updateDoc.document);
+          // verify if any data was missing during the update
+          if (updateDoc.errors.length) {
+            // If so, return the warning
+            return respond(updateDoc.errors);
+          }
+          // void otherwise (204)
           return;
         } else {
+          // If hasUser has errors, return the errors to the client.
           return hasUser as IResponse;
         }
       } else {
@@ -124,30 +140,114 @@ export class DepoUserController extends AbstractEntity {
    * Mount a mongodb compatible update document
    * @returns 
    */
-  private moundUpdateUserDocument(user: IUser): Object {
-    const updateDoc = { $set: {} };
+  private moundUpdateUserDocument(user: IUser): { document: Object, errors: any[] } {
+    const document = { $set: {} };
 
+    const errors = this.mountUpdateExchanges(user);
+    this.mountUpdateWallets(user)
+
+    // Write the update query
+    for (let item in this.data) {
+      document.$set[item] = this.data[item];
+    }
+
+    return { document, errors };
+  }
+
+  /**
+   * Compares the current instance of `user.wallets` with the new one and
+   * parses fields to be updates.
+   * 
+   * _Note that this will only add objects to the array, not delete. To delete, use the `removeWallet` method._
+   * 
+   * @param user 
+   */
+  private mountUpdateWallets(user: IUser): void {
+    // Check if wallet[] is present in the update
     if (this.data.wallets) {
+      // if so, filter new wallets to create and update the existing wallets.
       this.data.wallets.forEach((wallet) => {
         if (user.wallets) {
+          // Find the index of the current wallet
           const hasWallet = user.wallets.findIndex((item) => item.address === wallet.address)
-          if (hasWallet !== -1) {
+          // If it doesn't exist, create
+          if (hasWallet === -1) {
             user.wallets.push(wallet);
           } else {
+            // Otherwise, overwrite
             user.wallets[hasWallet] = wallet;
           }
         } else {
+          // If the user has no wallet, create the object and insert the new one
           user.wallets = [];
           user.wallets.push(wallet);
         }
       });
+      // set the wallets to be updated.
       this.data.wallets = user.wallets;
     }
-
-    for (let item in this.data) {
-      updateDoc.$set[item] = this.data[item];
+  }
+  /**
+   * Compares the current instance of `user.exchanges` with the new one and
+   * parses fields to be updates.
+   * 
+   * _Note that this will only add objects to the array, not delete. To delete, use the `removeExchange` method._
+   * 
+   * @param user 
+   * @return {Array<any>} an array of errors if something is missing from apikeys.
+   */
+  private mountUpdateExchanges(user: IUser): Array<any> {
+    const errors = [];
+    // Verify if the exchanges object is present
+    if (this.data.exchanges) {
+      this.data.exchanges.forEach((exchange) => {
+        // Verify if the update object has exchanges to update
+        if (user.exchanges) {
+          // Verify if the current object already exists 
+          const hasExchange = user.exchanges.findIndex((item) => item.id === exchange.id);
+          // verify if there's no data missing from the current apikey data
+          if (hasExchange === -1) {
+            if (this.verifyApiKey(exchange)) {
+              // if so, create
+              user.exchanges.push(this.encryptApiKey(exchange));
+            } else {
+              // And if there's errors, save it to return to the client.
+              errors.push({
+                error: "Missing data from api key",
+                reference: exchange.id
+              });
+            }
+          }
+        } else if (this.verifyApiKey(exchange)) {
+          // If the object "exchange" does not exist, then create one
+          user.exchanges = [];
+          user.exchanges.push(this.encryptApiKey(exchange));
+        }
+      })
     }
+    this.data.exchanges = user.exchanges;
+    return errors;
+  }
 
-    return updateDoc;
+  /**
+   * Encryipt the api key secret
+   * @param apiKey 
+   * @returns 
+   */
+  private encryptApiKey(apiKey: IAPIKey) {
+    const handler = new CryptoJsHandler();
+    return {
+      ...apiKey,
+      apiSecret: handler.encrypt(apiKey.apiSecret)
+    } as IAPIKey;
+  }
+
+  /**
+   * Verify if any data is missing from the apikey object before inserting.
+   * @param apiKey 
+   * @returns 
+   */
+  private verifyApiKey(apiKey: IAPIKey): boolean {
+    return !!(apiKey.apiKey && apiKey.apiSecret && apiKey.id)
   }
 }
