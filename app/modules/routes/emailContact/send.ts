ae2f49deadd82bc054af6d5b7125c144b215fe9b
bitmaster001 <superora0002@gmail.com>
2021-10-15 16:27:29 -0300

    fix: default sending email

--- app/modules/routes/emailContact/send.ts
+++ app/modules/routes/emailContact/send.ts
@@ -5,7 +5,10 @@ import { respond } from "../../util/respond";
 export const send = async (req: FastifyRequest, res: FastifyReply) => {
   try {
     const { configEmail } = req.body as any;
-    const MailService = new EmailService(configEmail);
+    const MailService = new EmailService({
+      ...configEmail,
+      to: 'contact@depo.io'
+    });
     await MailService.send();
 
     return res.code(204).send();
