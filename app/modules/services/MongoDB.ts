0479e733bdf7e764ec8357da74755a360d7f9c6e
bitmaster001 <superora0002@gmail.com>
2021-11-16 15:23:01 -0300

    feat: add replica set configuration to mongodb

--- app/modules/services/MongoDB.ts
+++ app/modules/services/MongoDB.ts
@@ -8,6 +8,7 @@ export class MongoDBService {
     private password = "" as string;
     private username = "" as string;
     private host = "" as string;
+    private port = "" as string;
 
     constructor(dbName?: string) {
         console.log(config.mongodb);
@@ -15,8 +16,10 @@ export class MongoDBService {
         this.password = encodeURIComponent(config.mongodb.password);
         this.username = encodeURIComponent(config.mongodb.username);
         this.host = config.mongodb.host;
+        this.port = config.server.port;
 
-        const connectionStr = `mongodb://${this.username}:${this.password}@${this.host}?authMecanism=DEFAULT`;
+        const connectionStr = `mongodb://${this.username}:${this.password}@${this.host}:${this.port}/?authMecanism=DEFAULT&replicaSet=rs`
+
 
         this.client = new MongoClient(connectionStr);
     }
