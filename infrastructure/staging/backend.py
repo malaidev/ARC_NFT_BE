751b97e74adf5bd3754fcfb1d3af9dea810ebc54
bitmaster001 <superora0002@gmail.com>
2022-02-14 07:31:42 +0800

    reduce replicate to test pipeline

--- infrastructure/staging/backend.py
+++ infrastructure/staging/backend.py
@@ -67,7 +67,7 @@ class Stack(core.Stack):
             "metadata": {"name": f"{props['namespace']}-deployment"},
             "spec": {
                 "selector": {"matchLabels": {"app.kubernetes.io/name": f"{props['namespace']}-app"}},
-                "replicas": 6,
+                "replicas": 5,
                 "template": {
                     "metadata": {"labels": {"app.kubernetes.io/name": f"{props['namespace']}-app"}},
                     "spec": {
