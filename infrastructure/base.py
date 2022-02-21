73d8ec331ce964973500cfb45488273269e8406f
bitmaster001 <superora0002@gmail.com>
2022-02-21 15:41:47 +0800

    Add test script before build docker image

--- infrastructure/base.py
+++ infrastructure/base.py
@@ -47,7 +47,9 @@ class Base(core.Stack):
                     },
                     "pre_build": {
                         "commands": [
+                            "echo '--------PREBUILD PHASE--------'",
                             "npm i -g aws-cdk",
+                            "npm test",
                             "pip3 install -U pip",
                             "pip3 install -r infrastructure/requirements.txt",
                             "aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com", # noqa
@@ -55,6 +57,7 @@ class Base(core.Stack):
                     },
                     "build": {
                         "commands": [
+                            "echo '--------POST-BUILD PHASE--------'",
                             "VERSION=`node -e \"console.log(require('./package.json').version);\"`",
                             "docker build --build-arg AWS_REGION=$REGION --build-arg ACCOUNT_ID=$ACCOUNT_ID -t $TAG:$VERSION .",
                             "docker tag $TAG:$VERSION $ECR:$VERSION",
