9ad6205a6b63a7670d1c6f7edba79a24ecf8701d
bitmaster001 <superora0002@gmail.com>
2022-03-04 00:15:08 +0800

    Add getOwners test

--- app/test/routes/nft/get.test.ts
+++ app/test/routes/nft/get.test.ts
@@ -5,10 +5,23 @@ let app = build();
 test("getItems API test [GET] [/]", async () => {
   const res = await app.inject({
     method: 'GET',
-    url: "http://localhost:3001/ws/v2/getItems",
+    url: "http://localhost:3001/ws/v2/items",
     headers: {
       'Access-Control-Allow-Origin': '*',
     }
   });
   expect(res.statusCode).toEqual(200);
 });
+
+
+test("getOwners API test [GET] [/]", async () => {
+  const res = await app.inject({
+    method: 'GET',
+    url: "http://localhost:3001/ws/v2/owners",
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    }
+  });
+  expect(res.statusCode).toEqual(200);
+});
+
