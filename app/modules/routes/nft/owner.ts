50f3d483c7f7fc83fb9ee59cbb6eaa6ed9346d32
bitmaster001 <superora0002@gmail.com>
2022-05-03 05:57:29 +0700

    hot fix upload profile

--- app/modules/routes/nft/owner.ts
+++ app/modules/routes/nft/owner.ts
@@ -23,7 +23,7 @@ export const uploadOwnerPhoto = async (req, res) => {
     ";base64," +
     Buffer.from(await req.body.photoFile.toBuffer()).toString("base64"); // access files
   const ctl = new NFTOwnerController();
-  const result = await ctl.updateOwnerPhoto(ownerId, photoBody);
+  const result = await ctl.updateOwnerPhoto(ownerId.toLowerCase(), photoBody);
   res.send(result);
 };
 
